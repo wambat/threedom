7d662621dbe945427265a5a84304ab83f4e9d637
Mike Konkov <noospheratum@gmail.com>
2015-09-29 21:09:38 +0200

    Creating sort of works

--- src/threedom/core.clj
+++ src/threedom/core.clj
@@ -6,47 +6,51 @@
   (clojure.pprint/pprint args)
   (clojure.lang.Reflector/invokeConstructor klass (into-array Object args)))
 
-(defn find-method [klass name pklass]
+(defn find-method [klass name pklasses]
+  (clojure.pprint/pprint "Find method")
+  (clojure.pprint/pprint klass)
+  (clojure.pprint/pprint name)
+  (clojure.pprint/pprint pklasses)
   (try
-    (if-let [m (.getMethod klass name (into-array [pklass]))]
+    (if-let [m (.getMethod klass name (into-array pklasses))]
       m)
     (catch Exception e
       (do
-        (if-let [spklass (.getSuperclass pklass)]
-          (if (not= klass spklass)
-            (find-method klass name spklass)))))))
+        (if-let [spklasses (map #(.getSuperclass %) pklasses)]
+          (if (not= pklasses spklasses)
+            (find-method klass name spklasses)))))))
 
 (defprotocol IRender
   (render [this]))
 
-(defn make-node! [owner klass konstructor & [setters children]]
+(defn make-node [klass konstructor & [setters children]]
   (clojure.pprint/pprint "Make node:")
   (clojure.pprint/pprint klass)
   (clojure.pprint/pprint "Konstructor:")
   (clojure.pprint/pprint konstructor)
   (let [node (apply construct klass konstructor)]
-    (doseq [[meth val] (into [] setters)]
-      (let [s (str "set" (name meth))
-            v (apply construct (first val) (second val))
-            met (find-method (.getClass node) s (.getClass v))]
+    (doseq [[meth vals] (into [] setters)]
+      (let [s (name meth)
+            vs (map #(if (and
+                          (sequential? %)
+                          (>= (count %) 2))
+                       (apply make-node %)
+                       %) vals)
+            met (find-method (.getClass node) s (map #(.getClass %) vs))]
         (clojure.pprint/pprint "Invoke")
         (clojure.pprint/pprint s)
         (clojure.pprint/pprint met)
         (clojure.pprint/pprint "params")
-        (clojure.pprint/pprint v)
+        (clojure.pprint/pprint vals)
+        (clojure.pprint/pprint vs)
         (clojure.pprint/pprint "on")
         (clojure.pprint/pprint node)
-        (.invoke met node (to-array [v]))
+        (.invoke met node (to-array vs))
         (clojure.pprint/pprint "S")
         ))
-    (clojure.pprint/pprint "attach")
-    (clojure.pprint/pprint owner)
-    (clojure.pprint/pprint node)
-    (.attachChild owner node)
-
     (doseq [c children]
-      (apply make-node! node c)
-      )))
+      (.attachChild node (apply make-node c)))
+    node))
 
 (defn materialize-diffs [cm owner old-state new-state options]
   (clojure.pprint/pprint "State from:")
@@ -54,7 +58,7 @@
   (clojure.pprint/pprint "State to:")
   (clojure.pprint/pprint new-state)
   (doseq [i new-state]
-    (apply make-node! owner i)))
+    (.attachChild owner (apply make-node i))))
 
 (defn handle-diffs
   ;; ([f target state]
